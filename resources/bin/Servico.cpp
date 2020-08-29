295a69c4104b292222f6223284160e35a924af09
takyorama <takyorama@gmail.com>
2020-08-29 11:43:10 -0300

    Atualização de Servico

--- resources/bin/Servico.cpp
+++ resources/bin/Servico.cpp
@@ -2,12 +2,12 @@
 
 using namespace Contabil::Model;
 
-Servico::Servico(){}
-Servico::Servico(int, std::string, float){}
+Servico::Servico(){ this->descricao = "";this->preco = 0; }
+Servico::Servico(int code, std::string descript, float price){this->descricao = descript ;this->preco = price;}
 
 /**********  GETTERS & SETTERS  **********/
-void Servico::setDescript(std::string){}
-void Servico::setPrice(float){}
+void Servico::setDescript(std::string descript){ this->descricao = descript; }
+void Servico::setPrice(float price){ this->preco = price; }
 
-std::string Servico::getDescript(){}
-float Servico::getPrice(){}
+std::string Servico::getDescript(){ return this->descricao; }
+float Servico::getPrice(){ return this->preco; }
